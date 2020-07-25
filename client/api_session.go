80f0485688c988a103586b15943dbc4e1865cf34
Jixun Wu <Jixun.Moe@gmail.com>
2020-07-25 22:02:40 +0100

    提供使用 API 获取 SessionID 的方法。

--- client/api_session.go
+++ client/api_session.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -14,6 +15,10 @@ func (c *Client) UpdateSession() error {
 	}
 
 	// Use API to fetch session id (fallback)
+	return c.UpdateSessionUsingAPI()
+}
+
+func (c *Client) UpdateSessionUsingAPI() error {
 	session, err := c.GetSessionTokenInfo()
 	if err == nil {
 		sessionID := strings.Replace(session.Session, "SessionID=", "", 1)
@@ -26,6 +31,7 @@ func (c *Client) UpdateSession() error {
 			Value: sessionID,
 		}
 		c.client.Jar.SetCookies(path, []*http.Cookie{cookie})
+		log.Printf("sessionID: %s\n", sessionID)
 	}
 
 	return nil
