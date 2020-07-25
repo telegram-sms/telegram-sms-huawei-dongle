c41ed68aa8a2a13eafd529d06e2074041f3b31e0
Jixun Wu <Jixun.Moe@gmail.com>
2020-07-25 17:00:46 +0100

    Don't print password to the logs.

--- client/api_user_test.go
+++ client/api_user_test.go
@@ -1,15 +1,12 @@
 package client
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
 func TestClient_Login(t *testing.T) {
-	fmt.Printf("SECRET_PASSWORD: %s\n", os.Getenv("SECRET_PASSWORD"))
-
 	c := &Client{}
 	err := c.Init(dongleURL)
 	assert.Nilf(t, err, "could not init")
